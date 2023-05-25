2ca3f5d5de17a6199e80c94fea7c80e61e6f7670
Noam Mirjani <Noamirjani@gmail.com>
2023-05-25 16:11:20 +0300

    add scrollable checkout

--- movie-app/src/componnets/Pages/Checkout.js
+++ movie-app/src/componnets/Pages/Checkout.js
@@ -5,6 +5,7 @@ import { useCart } from "../Context/CartProvider";
 import CartContent from "../Cart/CartContent";
 import CheckoutCartInfo from "../Checkout/CheckoutCartInfo";
 import CheckoutUserRegister from "../Checkout/CheckoutUserRegister";
+import '../../styles/Checkout.css';
 
 function Checkout() {
     const cart = useCart();
@@ -14,29 +15,29 @@ function Checkout() {
     return (
         <Container fluid>
             <Row>
-                <Row className="row-cols-2">
-                    <Col xs={12}  md={mdScreenColSize}>
+                <Row className="row-cols-2 mb-2">
+                    <Col xs={12} md={mdScreenColSize}>
                         <Container>
                             <CheckoutMessage />
                         </Container>
                     </Col>
-
                     <Col xs={12} md={mdScreenColSize} className="d-flex justify-content-center align-items-center">
-                        <div className="d-flex justify-content-center">
+                        <div className="d-flex justify-content-center cart-container">
                             <Container>
                                 <CartContent />
                             </Container>
                         </div>
                     </Col>
                 </Row>
-                {renderCartData &&
-                <Row className="row-cols-2">
-                    <Col xs={12}  md={mdScreenColSize}>
-                        <CheckoutCartInfo />
-                        <CheckoutUserRegister />
-                    </Col>
-                    <Col xs={12} md={mdScreenColSize} className="bg-info d-none"></Col>
-                </Row>}
+                {renderCartData && (
+                    <Row className="row-cols-2">
+                        <Col xs={12} md={mdScreenColSize}>
+                            <CheckoutCartInfo />
+                            <CheckoutUserRegister />
+                        </Col>
+                        <Col xs={12} md={mdScreenColSize} className="d-none d-md-block"></Col>
+                    </Row>
+                )}
             </Row>
         </Container>
     );
